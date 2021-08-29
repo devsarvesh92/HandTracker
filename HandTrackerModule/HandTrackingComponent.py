128d6e5a851826b298336e08b24fcbe55ef28698
Sarvesh Sawant <sarveshdev92@gmail.com>
2021-08-30 01:27:54 +0530

    Commit Module

--- HandTrackerModule/HandTrackingComponent.py
+++ HandTrackerModule/HandTrackingComponent.py
@@ -3,24 +3,47 @@ import mediapipe as mp
 import time
 
 
-cap = cv2.VideoCapture(0)
+class handTracker:
+    def __init__(self, mode=False,maxHands = 2, detectionConfidence = 0.5, trackConfidence = 0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionConfidence = detectionConfidence
+        self.trackConfidence = trackConfidence
+        self.mp_draw = mp.solutions.drawing_utils
 
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands()
+    def startHandTracking(self, cameraId=0,handNumber=1):
+        cap = cv2.VideoCapture(cameraId)
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands()
 
-mp_draw = mp.solutions.drawing_utils
+        while True:
+            success,img = cap.read()
+            imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+            results = self.hands.process(imgRGB)
+            if results.multi_hand_landmarks:
+                for handLms in results.multi_hand_landmarks:
+                    self.mp_draw.draw_landmarks(img,handLms,self.mp_hands.HAND_CONNECTIONS)
+            cv2.imshow("Tracker",img)
+            cv2.waitKey(1)
 
-while True:
-    success,img = cap.read()
-    print(success)
+def main():
+    handTrackerMod = handTracker()
+    handTrackerMod.startHandTracking(0,1)
 
-    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
+if __name__ == "__main__":
+    main()
 
 
-    if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks:
-            mp_draw.draw_landmarks(img,handLms,mp_hands.HAND_CONNECTIONS)
 
-    cv2.imshow("Tracker",img)
-    cv2.waitKey(1)
+
+    
+    
+        
+
+
+
+
+
+
+
+
